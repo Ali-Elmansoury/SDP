4dc2342de49eafd64af61201faeca93fa54cadac
Ali Elmansoury <73143531+Ali-Elmansoury@users.noreply.github.com>
2023-10-29 18:28:25 +0200

    Add files via upload

---
+++ Assignment 2/SDP-Assignment2/SDP-Assignment2/Jacket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SDP_Assignment2
+{
+    interface Jacket
+    {
+        public void Material();
+
+        public void Size();
+
+        public void Gender();
+    }
+}
